ce4a7760b7c1928f6db2ca9c90ec3e8a1e227b86
Keegan White <keegan28white@gmail.com>
2023-07-24 14:21:33 -0500

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace StaticExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+
+using StaticExercise;
+
+Console.WriteLine("Fahrenheit to Celsius");
+Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+
+Console.WriteLine("Celsius to Fahrenheit");
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(0));
